42a7d844472dfbdfa356d675b1f844794c26d6db
PA1NxLETHALZz <73175338+PA1NxLETHALZz@users.noreply.github.com>
2020-10-22 16:53:32 +0100

    Update pa1nxl3thalz.js

--- pa1nxl3thalz.js
+++ pa1nxl3thalz.js
@@ -43,6 +43,30 @@ client.on("message", (message) => {
  
 });
 
+// Set the prefix
+
+const prefix = "!";
+
+client.on("message", (message) => {
+ 
+  // Exit and stop if it's not there
+ 
+  if (!message.content.startsWith(prefix)) return;
+
+  if (message.content.startsWith(prefix + "ping")) {
+   
+    message.channel.send("pong!");
+   
+  } else
+   
+  if (message.content.startsWith(prefix + "foo")) {
+   
+    message.channel.send("bar!");
+   
+  }
+ 
+});
+
 // THIS  MUST  BE  THIS  WAY
 
 client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
