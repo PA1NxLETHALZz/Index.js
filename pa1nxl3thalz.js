852682593e7bbfde069021f073e53ffc03126ed0
PA1NxLETHALZz <73175338+PA1NxLETHALZz@users.noreply.github.com>
2020-10-22 15:59:23 +0100

    Update pa1nxl3thalz.js

--- pa1nxl3thalz.js
+++ pa1nxl3thalz.js
@@ -22,7 +22,21 @@ client.on('message', message => {
 
 });
 
- 
+static void UpdatePresence()
+{
+    DiscordRichPresence discordPresence;
+    memset(&discordPresence, 0, sizeof(discordPresence));
+    discordPresence.state = "Playing Warzone";
+    discordPresence.details = "1st - God Tier ";
+    discordPresence.startTimestamp = 1507665886;
+    discordPresence.endTimestamp = 11507665886;
+    discordPresence.largeImageText = "Numbani";
+    discordPresence.smallImageText = "God Tier - Level 100";
+    discordPresence.partyId = "ae488379-351d-4a4f-ad32-2b9b01c91657";
+    discordPresence.partySize = 1;
+    discordPresence.partyMax = 100;
+    Discord_UpdatePresence(&discordPresence);
+}
 
 // THIS  MUST  BE  THIS  WAY
 
