5fa93be91c729100d2980a0c5edfd933e980a747
PA1NxLETHALZz <73175338+PA1NxLETHALZz@users.noreply.github.com>
2020-10-22 16:08:11 +0100

    Update pa1nxl3thalz.js

--- pa1nxl3thalz.js
+++ pa1nxl3thalz.js
@@ -2,19 +2,27 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
+ 
+
 client.on('ready', () => {
 
     console.log('I am ready!');
 
 });
 
+ 
+
 client.on('message', message => {
 
-    if (message.content === 'Ping') {
+    if (message.content === 'ping') {
 
-       message.reply('Pong');
+       message.reply('pong');
 
-}
+       }
+
+});
+
+ 
 
 // THIS  MUST  BE  THIS  WAY
 
