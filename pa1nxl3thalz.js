84dcf3c6daaf589f3e65752d667a7dfe42ad8e97
PA1NxLETHALZz <73175338+PA1NxLETHALZz@users.noreply.github.com>
2020-10-23 11:07:23 +0100

    Update pa1nxl3thalz.js

--- pa1nxl3thalz.js
+++ pa1nxl3thalz.js
@@ -1,12 +1,16 @@
 const Discord = require('discord.js');
 
-const client = new Discord.Client();
-
- 
+const client = new Discord.Client(); 
 
 client.on('Ready', () => {
 
     console.log('I am ready!');
+ 
+ client.on('message', msg => {    
+  if (msg.content === `"Your message!"`) {      
+    msg.channel.type === (`"dm"`) + msg.author.sendMessage(`"Your other message"`) 
+  }
+}
 
 });
 
