198508fb1df619d30afcd991fdc07a3b41b78dcc
PA1NxLETHALZz <73175338+PA1NxLETHALZz@users.noreply.github.com>
2020-10-23 08:51:08 +0100

    Update pa1nxl3thalz.js

--- pa1nxl3thalz.js
+++ pa1nxl3thalz.js
@@ -28,6 +28,89 @@ client.on("message", (message) => {
  
 });
 
+import discord
+
+from discord.ext.commands import commands,has_permissions, MissingPermissions
+
+import json
+
+with open('reports.json', encoding='utf-8') as f:
+
+  try:
+    report = json.load(f)
+
+  except ValueError:
+
+    report = {}
+
+    report['users'] = []
+
+client = discord.ext.commands.Bot(command_prefix = '?')
+
+@client.command(pass_context = True)
+
+@has_permissions(manage_roles=True, ban_members=True)
+
+async def warn(ctx,user:discord.User,*reason:str):
+
+  if not reason:
+
+    await client.say("Please provide a reason")
+
+    return
+  reason = ' '.join(reason)
+
+  for current_user in report['users']:
+  
+    if current_user['name'] == user.name:
+    
+      current_user['reasons'].append(reason)
+
+      break
+
+  else:
+  
+    report['users'].append({
+     
+      'name':user.name,
+     
+      'reasons': [reason,]
+     
+    })
+
+  with open('reports.json','w+') as f:
+
+    json.dump(report,f)
+
+@client.command(pass_context = True)
+
+async def warnings(ctx,user:discord.User):
+
+  for current_user in report['users']:
+  
+    if user.name == current_user['name']:
+    
+      await client.say(f"{user.name} has been reported {len(current_user['reasons'])} times : {','.join(current_user['reasons'])}")
+
+      break
+  else:
+  
+    await client.say(f"{user.name} has never been reported")  
+
+@warn.error
+
+async def kick_error(error, ctx):
+
+  if isinstance(error, MissingPermissions):
+  
+      text = "Sorry {}, you do not have permissions to do that!".format(ctx.message.author)
+
+      await client.send_message(ctx.message.channel, text)   
+
+client.run("BOT_TOKEN")
+
+});
+
 client.on("message", (message) => {
  
   if (message.content.startsWith("Ping")) {
