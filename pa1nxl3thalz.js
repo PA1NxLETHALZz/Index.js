a9708082c7820c60704bad00f65fb7b9027cf089
PA1NxLETHALZz <73175338+PA1NxLETHALZz@users.noreply.github.com>
2020-10-23 10:08:49 +0100

    Update pa1nxl3thalz.js

--- pa1nxl3thalz.js
+++ pa1nxl3thalz.js
@@ -67,6 +67,23 @@ client.on("message", (message) => {
  
 });
 
+if (cmd === `!dm`) {
+ let dUser =
+  message.guild.member(message.mentions.users.first()) ||
+  message.guild.members.get(args[0]);
+ if (!dUser) return message.channel.send("Can't find user!");
+ if (!message.member.hasPermission('ADMINISTRATOR'))
+  return message.reply("You can't you that command!");
+ let dMessage = args.join(' ').slice(22);
+ if (dMessage.length < 1) return message.reply('You must supply a message!');
+
+ dUser.send(`${dUser} A moderator from WP Coding Club sent you: ${dMessage}`);
+
+ message.author.send(
+  `${message.author} You have sent your message to ${dUser}`
+ );
+}
+
 // THIS  MUST  BE  THIS  WAY
 
 client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
